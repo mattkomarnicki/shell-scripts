17eb3ee1e80f2a8790b229d94fd3ea96b558142e
Matthew Komarnicki <slick@slick.pl>
2015-11-29 16:28:14 +1000

    Activate specific theme and plugins within .zshrc file

--- _internal/install-oh-my-zsh.sh
+++ _internal/install-oh-my-zsh.sh
@@ -2,6 +2,12 @@
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+echo "Activating theme..."
+sed -i '' -e 's/^ZSH_THEME=.*/ZSH_THEME=\"simple\"/' ~/.zshrc
+
+echo "Activating plugins..."
+sed -i '' -e 's/^plugins=.*/plugins=(git, sublime)/' ~/.zshrc
+
 echo "Great! Oh My Zsh is installed."
 
 exit 0
